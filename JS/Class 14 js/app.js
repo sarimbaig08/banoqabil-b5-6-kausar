b2ec7a6079822ef8d9ea4ebb4984d1421db92d2c
sarimbaig08 <sarimbaig321@gmail.com>
2025-08-18 08:58:18 -0700

    var names, types, BODMAS

---
+++ JS/Class 14 js/app.js
@@ -0,0 +1,55 @@
+// alert("Connected")
+
+
+// Illegal Variable Names
+
+//Rule no. 01: #name special chac can't be used in start and middle except $ _
+// var #name = "sarim"
+
+//Rule no.02: 2name can't use number in start
+// var 2name = "sarim"
+
+//Rule no.03: can't use js reserved words
+//  var false = "isSuccess"
+
+ //Rule no.04 can't give space between variable
+//  var userName = "sarim"
+
+
+
+//  value re-assignment |  variable re-declaration
+// var:  true           |           true
+// let:  true           |           false
+// const: false         |           false
+
+var userName; // var declaration
+userName = "Mirza Sarim Baig" // value asignment
+
+
+// const value = "hello world"
+//  value = "hello js"
+
+// console.log(value)
+
+
+
+//Arthmetic Operators
+
+// + - * /
+// BODMAS Rule
+
+let num1 = 10
+let num2 = "abc"
+let num3 = 30
+
+// console.log(num1 - num2 * num3 + (num1 + 2))
+
+
+// console.log(num1 + num2)
+
+//Pre post inc
+console.log(num1++)
+console.log(num1)
+// console.log(++num1)
+// console.log(++num1)
+
--- JS/app.js
+++
@@ -1,5 +0,0 @@
-  console.log("This is my js first class");
-      console.log("This is my js class", 1);
-      console.log(10);
-
-      alert("Login Succes");
